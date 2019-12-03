5b1bc64096f70485b5643de867ba33ff9c8f9628
Akshay Guthal <akguthal@gmail.com>
2019-12-03 15:03:54 -0500

    frontend initial code with readme

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Tiling Networks
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
