cfe9e801a1c08716a494996f49091758728fc21a
nwarford <noel.warford95@gmail.com>
2019-12-05 13:04:32 -0500

    started hierarchy tree gen algorithm + updated todo

--- dataset/hierarchy-example.py
+++ dataset/hierarchy-example.py
@@ -3,6 +3,15 @@ from networkx.algorithms import community
 import itertools
 import psycopg2 as db
 
+def minLength(list) :
+    minLength = None
+    for sublist in list:
+        if minLength == None:
+            minLength = len(sublist)
+        elif len(sublist) < minLength :
+            minLength = len(sublist)
+    return minLength
+
 # INITIALIZE TABLES
 # conn = db.connect(user='tiling_networks_user', password='password', dbname='tiling_networks', host='127.0.0.1', port='5432')
 # cursor = conn.cursor()
@@ -17,5 +26,17 @@ comp = community.girvan_newman(G)
 # limited = itertools.takewhile(lambda c: len(c) <= k, comp)
 # for communities in limited:
 #     community_list = tuple(sorted(c) for c in communities)
-for communities in itertools.islice(comp, k):
-    print(tuple(sorted(c) for c in communities))
+# for communities in itertools.islice(comp, k):
+#     print(tuple(sorted(c) for c in communities))
+
+done = False
+niters = 0
+prev_list = None
+while not done:
+    niters += 1
+    curr_list = tuple(sorted(c) for c in next(comp))
+    current_min = minLength(curr_list)
+    print(current_min)
+    print(current_list)
+    if current_min == 1 :
+        done = True
