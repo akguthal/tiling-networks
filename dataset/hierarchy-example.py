d4c293a9949eec6b21590e570da7eff0d8c5719e
nwarford <noel.warford95@gmail.com>
2019-12-05 14:57:19 -0500

    implementing GN on larger sets - very slow!

--- dataset/hierarchy-example.py
+++ dataset/hierarchy-example.py
@@ -19,24 +19,36 @@ def minLength(list) :
 # conn.commit()
 
 # GENERATE DUMMY DATA
-G = nx.read_adjlist("./hierarchy.txt")
+f = open("email-Eu-core.txt","r")
+o = open("email-Eu-core-short.txt","w")
+for i in range(2000) : # this takes a few minutes to run, and this is the smallest dataset we have
+    lineout = f.readline()
+    print(lineout)
+    o.write(lineout)
 
-k = 3
+G = nx.read_adjlist("./email-Eu-core-short.txt")
+print("graph assembled")
+print("number of nodes:",nx.number_of_nodes(G))
+print("number of edges:",nx.number_of_edges(G))
+
+
 comp = community.girvan_newman(G)
+
+k = 10
 # limited = itertools.takewhile(lambda c: len(c) <= k, comp)
 # for communities in limited:
 #     community_list = tuple(sorted(c) for c in communities)
-# for communities in itertools.islice(comp, k):
-#     print(tuple(sorted(c) for c in communities))
+for communities in itertools.islice(comp, k):
+    print(tuple(sorted(c) for c in communities))
 
-done = False
-niters = 0
-prev_list = None
-while not done:
-    niters += 1
-    curr_list = tuple(sorted(c) for c in next(comp))
-    current_min = minLength(curr_list)
-    print(current_min)
-    print(current_list)
-    if current_min == 1 :
-        done = True
+# done = False
+# niters = 0
+# prev_list = None
+# while not done:
+#     niters += 1
+#     curr_list = tuple(sorted(c) for c in next(comp))
+#     current_min = minLength(curr_list)
+#     print(current_min)
+#     print(current_list)
+#     if current_min == 1 :
+#         done = True
